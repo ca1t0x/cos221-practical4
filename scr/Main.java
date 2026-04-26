a27a50e9a61ea219b358fdb1bccfaa6f6dcc6887
kiaraajodhaparsadh <kiaraajodhaparsadh@gmail.com>
2026-04-26 12:49:48 +0200

    Update Main.java

--- scr/Main.java
+++ scr/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("COS221 Practical 4 Started");
+        System.out.println("COS221 Practical 4");
     }
 }
