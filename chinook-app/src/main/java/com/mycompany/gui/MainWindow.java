df9f2a017fe6e1d3942c238bcbb6436a36271b85
kiaraajodhaparsadh <kiaraajodhaparsadh@gmail.com>
2026-04-27 11:55:29 +0200

    Configured Maven build to run main application using exec plugin

--- chinook-app/src/main/java/com/mycompany/gui/MainWindow.java
+++ chinook-app/src/main/java/com/mycompany/gui/MainWindow.java
@@ -1,6 +1,8 @@
 package com.mycompany.gui;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class MainWindow extends JFrame {
 
@@ -17,6 +19,7 @@ public class MainWindow extends JFrame {
         tabs.addTab("Report", new JPanel());
         tabs.addTab("Notifications", new JPanel());
         tabs.addTab("Recommendations", new JPanel());
+        
 
         add(tabs);
     }
