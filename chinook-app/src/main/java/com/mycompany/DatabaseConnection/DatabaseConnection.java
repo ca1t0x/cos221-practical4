7e6f898c32301df7cd5c7f7ed397c938ea39bf76
kachowski.95 <caitlinn.moodley@gmail.com>
2026-04-28 21:50:03 +0200

    Refactor DatabaseConnection to use environment variables

--- chinook-app/src/main/java/com/mycompany/DatabaseConnection/DatabaseConnection.java
+++ chinook-app/src/main/java/com/mycompany/DatabaseConnection/DatabaseConnection.java
@@ -1,25 +1,28 @@
-package com.mycompany.DatabaseConnection;
+package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static Connection connection = null;
 
-    public static Connection getConnection() {
-        try {
-            String url = "jdbc:mysql://localhost:3306/chinook"; // change later if needed
-            String user = "root"; // change if needed
-            String password = "password"; // change if needed
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            String proto    = System.getenv("CHINOOK_DB_PROTO");
+            String host     = System.getenv("CHINOOK_DB_HOST");
+            String port     = System.getenv("CHINOOK_DB_PORT");
+            String name     = System.getenv("CHINOOK_DB_NAME");
+            String username = System.getenv("CHINOOK_DB_USERNAME");
+            String password = System.getenv("CHINOOK_DB_PASSWORD");
 
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to database!");
-            return conn;
-
-        } catch (SQLException e) {
-            System.out.println("Database connection failed!");
-            e.printStackTrace();
-            return null;
+            if ("jdbc:sqlite".equals(proto)) {
+                connection = DriverManager.getConnection(proto + ":" + name);
+            } else {
+                String url = proto + "://" + host + ":" + port + "/" + name;
+                connection = DriverManager.getConnection(url, username, password);
+            }
         }
+        return connection;
     }
 }
